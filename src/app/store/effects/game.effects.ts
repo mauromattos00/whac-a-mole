f649b05dbed1eff611fa9bb4ebeb0ce072e5f9f3
Mauro Mattos <mauromattos00@gmail.com>
2022-11-22 00:11:56 -0300

    feat: implement game start effect

--- src/app/store/effects/game.effects.ts
+++ src/app/store/effects/game.effects.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
+import {
+  delay,
+  tap,
+} from 'rxjs/operators';
 import * as gameActions from '@store/actions/game.actions';
 import { GameFacadeService } from '@store/facades/game-facade.service';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class GameEffects {
@@ -9,5 +14,19 @@ export class GameEffects {
     private actions$: Actions,
     private gameFacadeService: GameFacadeService,
   ) { }
+
+  startGame$ = createEffect(() => this.actions$.pipe(
+    ofType(gameActions.startGame),
+    tap(() => {
+      this.gameFacadeService.resetScore();
+      this.gameFacadeService.startTimer();
+      this.gameFacadeService.showMoles();
+    }),
+    delay(environment.gameDuration),
+    tap(() => {
+      this.gameFacadeService.checkHighestScore();
+      this.gameFacadeService.finishGame();
+    })
+  ), { dispatch: false });
 
 }
